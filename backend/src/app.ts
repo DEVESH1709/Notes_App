3257207ec4e0d1709ddc0b08ba3300f9fe347370
DEVESH1709 <deveshkesharwani2003@gmail.com>
2025-08-29 22:24:17 +0530

    feat: set up routing for authentication and notes management

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,8 @@ import express from "express";
 import cors from 'cors';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
-
+import authRoutes from "./routes/authRoutes"
+import noteRoutes from "./routes/noteRoutes"
 
 dotenv.config();
 const app = express();
@@ -10,6 +11,8 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use('/api/auth', authRoutes);
+app.use('/api/notes', noteRoutes);
 
 app.get('/',(req,res)=>{
     res.send('Api is running');
--- backend/src/routes/authRoutes.ts
+++ backend/src/routes/authRoutes.ts
@@ -5,3 +5,5 @@ const router  =  express.Router();
 router.post('/request-otp', requestOTP);
 router.post('/verify-otp', verifyOTP);
 router.post('/google-login', googleLogin);
+
+export default router;
